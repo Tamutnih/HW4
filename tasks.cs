8afa6905155f347be6e8d60f42fc091b0b140fd2
tatyana mutnykh <renu07@list.ru>
2022-08-17 22:03:43 +0500

    Решение

--- tasks.cs
+++ tasks.cs
@@ -20,25 +20,42 @@
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 
-Console.Write("Введите число N: ");
-int numberN = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
 
 
-  int SumNumber(int numberN){
+//   int SumNumber(int numberN){
     
-    int counter = Convert.ToString(numberN).Length;
-    int advance = 0;
-    int result = 0;
+//     int counter = Convert.ToString(numberN).Length;
+//     int advance = 0;
+//     int result = 0;
 
 
-    for (int i = 0; i < counter; i++){
-      advance = numberN - numberN % 10;
-      result = result + (numberN - advance);
-      numberN = numberN / 10;
-    }
-   return result;
-  }
+//     for (int i = 0; i < counter; i++){
+//       advance = numberN - numberN % 10;
+//       result = result + (numberN - advance);
+//       numberN = numberN / 10;
+//     }
+//    return result;
+//   }
 
 
-int sumNumber = SumNumber(numberN);
-Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+// int sumNumber = SumNumber(numberN);
+// Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + ",");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
